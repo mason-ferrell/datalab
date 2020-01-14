bf4fc811f9687215c9dd956f364bee77d02854e7
Mason Ferrell <thfe9880@colorado.edu>
2020-01-14 01:45:17 +0000

    First Commit

--- .ipynb_checkpoints/bits-checkpoint.c
+++ .ipynb_checkpoints/bits-checkpoint.c
@@ -1,7 +1,7 @@
 /* 
  * CS:APP Data Lab 
  * 
- * Mason Ferrell, 108-42-4192
+ * <Please put your name and userid here>
  * 
  * bits.c - Source file with your solutions to the Lab.
  *          This is the file you will hand in to your instructor.
